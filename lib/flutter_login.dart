b1d379b321d2f7c13a103b5a6a19638c2bb87078
mike doornenbal <mikedoornenbal9@icloud.com>
2024-02-06 16:05:46 +0100

    fix: added Ci and linter

--- lib/flutter_login.dart
+++ lib/flutter_login.dart
@@ -1,9 +1,10 @@
+///
 library flutter_login;
 
-export 'src/config/spacer_options.dart';
 export 'src/config/login_options.dart';
+export 'src/config/spacer_options.dart';
+export 'src/service/login_validation.dart';
+export 'src/service/validation.dart';
 export 'src/widgets/email_password_login.dart';
 export 'src/widgets/forgot_password_form.dart';
-export 'src/service/validation.dart';
-export 'src/service/login_validation.dart';
 export 'src/widgets/mfa_widget.dart';
