e8f3292dc7e36c7ee9f0fd57a98d8d38cdaf38a9
mike doornenbal <mikedoornenbal9@icloud.com>
2024-04-02 15:41:36 +0200

    fix: add default loginoptions for email and password fields

--- lib/src/config/login_options.dart
+++ lib/src/config/login_options.dart
@@ -35,6 +35,17 @@ class LoginOptions {
     this.forgotPasswordSpacerOptions = const ForgotPasswordSpacerOptions(),
   });
 
+  factory LoginOptions.defaults() => const LoginOptions(
+        emailDecoration: InputDecoration(
+          labelText: 'Email',
+          hintText: 'Enter your email',
+        ),
+        passwordDecoration: InputDecoration(
+          labelText: 'Password',
+          hintText: 'Enter your password',
+        ),
+      );
+
   /// Builds the login button.
   final ButtonBuilder loginButtonBuilder;
 
